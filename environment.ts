c34f794588a2fa21dc19900ed529730b275fa060
Hari prasad P <hariprasad.1si22cs404@gmail.com>
2025-04-05 19:35:54 +0530

    feat: update Azure workflow configuration and add OpenAPI documentation for Hacker News API

--- environment.ts
+++ environment.ts
@@ -1,1 +1,14 @@
 export const Private_Secret_Key = process.env.JWT_SECRET_KEY || process.exit(1);
+
+export const databaseUrl =
+  process.env.DATABASE_URL ||
+  (() => {
+    console.error("DATABASE_URL is not set! Exiting...");
+    process.exit(1);
+  })();
+export const directUrl =
+  process.env.DIRECT_URL ||
+  (() => {
+    console.error("DIRECT_URL is not set! Exiting...");
+    process.exit(1);
+  })();
